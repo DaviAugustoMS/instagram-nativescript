2d4a679e0f3c8869c734f3ab2f833b97b91cc97d
Davi Augusto <daviaugusto.developer@gmail.com>
2021-06-06 17:23:52 -0300

    fix: create app

---
+++ src/components/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <Page>
+    <ActionBar>
+      <StackLayout>
+        <Image class="logo" src="~/assets/instagram.png" />
+      </StackLayout>
+    </ActionBar>
+    <ScrollView>
+      <StackLayout>
+        <Post v-for="(item, index) in posts" :key="index" :Image="item.Image" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script lang="ts">
+//@ts-ignore
+import Post from "./Post";
+
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          Image:
+            "https://s2.glbimg.com/iDz2P4tP7Pns9fM_9Zi4ebtXuZI=/smart/e.glbimg.com/og/ed/f/original/2020/11/24/veneza-anuncia-taxa-de-entrada-para-turistas-a-partir-de-2022.jpg",
+        },
+        {
+          Image:
+            "https://etacanadavisa.com.br/main/wp-content/uploads/2018/05/image3-3-830x552.jpg",
+        },
+        {
+          Image:
+            "https://nespy2aub3if2yow3fcm0t5y-wpengine.netdna-ssl.com/files/2013/11/conhe%C3%A7a-londres-635x359.jpg",
+        },
+        {
+          Image:
+            "https://coisasdojapao.com/wp-content/uploads/2017/11/templo-japao.jpg",
+        },
+      ],
+    };
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #f5f5f5;
+  color: #ffffff;
+}
+
+.logo {
+  width: 100;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,56 @@
+<template>
+  <StackLayout class="post">
+    <FlexBoxLayout class="post__profile">
+      <StackLayout class="post__imgProfile">
+        <Image src="~/assets/davi.png" style="width: 100%" />
+      </StackLayout>
+      <Label text="daviaugusto.me" />
+    </FlexBoxLayout>
+    <Image :src="Image" class="post__image" />
+    <FlexboxLayout class="post__desc">
+      <Label
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla libero mi, placerat eu lacus vel, vestibulum varius ipsum."
+        textWrap
+      />
+    </FlexboxLayout>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  props: {
+    Image: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.post {
+  margin-top: 30;
+
+  &__profile {
+    padding: 0 10 10 10;
+
+    label {
+      margin-left: 10;
+      font-size: 13;
+      font-weight: 500;
+    }
+  }
+
+  &__imgProfile {
+    width: 35;
+    height: 35;
+    border-radius: 100;
+    overflow: hidden;
+  }
+
+  &__desc {
+    padding: 10;
+
+    label {
+      font-size: 12;
+    }
+  }
+}
+</style>
